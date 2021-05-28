7a4fc9449c1cfa0e6fb336bb3c31a015fc83c16c
Sergey Taranov <s.taranov@treatstock.com>
2021-05-28 23:14:28 +0300

    Refactor

--- src/LogTrait.php
+++ src/LogTrait.php
@@ -6,17 +6,8 @@ use Exception;
 
 trait LogTrait
 {
-    private $lastError;
-
-    public function getLastError(): ?string
-    {
-        return $this->lastError;
-    }
-
     public function logString($errorMessage): void
     {
-        $this->lastError = $errorMessage;
-
         /** @noinspection ForgottenDebugOutputInspection */
         error_log($errorMessage);
     }
